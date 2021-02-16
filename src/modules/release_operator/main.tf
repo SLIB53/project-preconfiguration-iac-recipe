8f230db4b5ffe0d50d19e6394ffccc64d1d69d67
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2021-02-16 09:46:51 +0000

    Fix release operator iam binding

--- src/modules/release_operator/main.tf
+++ src/modules/release_operator/main.tf
@@ -1,24 +1,15 @@
-data "google_iam_policy" "release_operator" {
-  binding {
-    role = "roles/storage.objectAdmin"
-
-    members = [
-      google_service_account.release_operator.email
-    ]
-  }
-
-  # TODO: pass list of additional roles
-}
-
 resource "google_service_account" "release_operator" {
   account_id   = var.release_operator_sa_id
   display_name = "Release Operator Service Account"
   description  = "A service account authorized for CI/CD"
 }
 
-resource "google_service_account_iam_policy" "release_operator" {
-  service_account_id = google_service_account.release_operator.name
-  policy_data        = data.google_iam_policy.release_operator.policy_data
+resource "google_project_iam_binding" "release_operator_cloud_storage" {
+  role = "roles/storage.objectAdmin"
+
+  members = [
+    "serviceAccount:${google_service_account.release_operator.email}"
+  ]
 }
 
 # TODO: create release operator key
