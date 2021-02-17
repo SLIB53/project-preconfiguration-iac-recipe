413bc7272e6f86778256011e9849a9fe45d7484e
Akil Krishnan <akil.krishnan.slib53@gmail.com>
2021-02-17 06:52:09 -0600

    Improve README, remove sa key from recipe (#5)

--- infra/modules/release_operator/main.tf
+++ infra/modules/release_operator/main.tf
@@ -3,7 +3,3 @@ resource "google_service_account" "release_operator" {
   display_name = "Release Operator Service Account"
   description  = "This service account is authorized for deployments."
 }
-
-resource "google_service_account_key" "release_operator_credentials" {
-  service_account_id = google_service_account.release_operator.name
-}
--- infra/modules/release_operator/outputs.tf
+++ infra/modules/release_operator/outputs.tf
@@ -1,3 +1,7 @@
+output "service_account" {
+  value = google_service_account.release_operator.email
+}
+
 output "as_member" {
   value = "serviceAccount:${google_service_account.release_operator.email}"
 }
